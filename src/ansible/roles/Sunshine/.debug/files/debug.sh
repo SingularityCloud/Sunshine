fa301faf212ff06163b35e992abc999d35b90cc0
Source Graph A.I <dev@source.unisocial.net>
2023-07-28 13:20:47 -0500

    try

--- src/ansible/roles/Sunshine/.debug/files/debug.sh
+++ src/ansible/roles/Sunshine/.debug/files/debug.sh
@@ -2,5 +2,7 @@
 sudo gnome-shell --replace &
 
 # Logout
+# sch: https://www.google.com/search?q=ubuntu+gnome-session-quit
 # https://www.makeuseof.com/how-to-log-out-ubuntu-linux-terminal/
+# https://askubuntu.com/questions/1357359/gnome-software-sorry-something-went-wrong-the-name-n-nnnn-was-not-provided-b
 gnome-session-quit --no-prompt
